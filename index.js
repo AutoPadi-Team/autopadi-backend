80a16f0a376d71785625114b19863ea994d0d003
Buertey Accam <111708279+george-accam@users.noreply.github.com>
2025-12-19 01:13:12 +0000

    Add CI/CD pipeline

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ dotenv.config();
 connectDB();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
