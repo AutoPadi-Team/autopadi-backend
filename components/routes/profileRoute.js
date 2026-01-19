f89b7d869444b0e2aea1cebad33ab2adf8a14f68
Buertey Accam <111708279+george-accam@users.noreply.github.com>
2026-01-19 23:38:18 +0000

    update

--- components/routes/profileRoute.js
+++ components/routes/profileRoute.js
@@ -5,7 +5,7 @@ const { verifyToken } = require("../middleware/authenticate");
 const upload = require("../middleware/multer");
 
 router.put("/update-profile/:userId", verifyToken, upload.single("image"), updateProfile); // update user profile
-router.get("/user-profile/:userId", verifyToken, getUserProfile); // get user profile by user id
+router.get("/user-profile/:userId", verifyToken, getUserProfile); // get user profile by id
 router.get("/users-profile", verifyToken, getAllProfile); // get all profiles
 
 module.exports = router;
